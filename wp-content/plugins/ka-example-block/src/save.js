debc63de2215c68193bfc73562cf8109b6ae37f2
victorkane <victorkane@gmail.com>
2023-03-15 10:24:45 -0300

    build (Adding align controls to the Block Toolbar): align style serialized and saved to database, now works in front end as well as editor

--- wp-content/plugins/ka-example-block/src/save.js
+++ wp-content/plugins/ka-example-block/src/save.js
@@ -17,8 +17,13 @@ import { useBlockProps, RichText } from "@wordpress/block-editor";
  */
 export default function save({ attributes }) {
 	const blockProps = useBlockProps.save();
-
+	const { content, align } = attributes;
 	return (
-		<RichText.Content {...blockProps} tagName="p" value={attributes.content} />
+		<RichText.Content
+			{...blockProps}
+			tagName="p"
+			value={content}
+			style={{ textAlign: align }}
+		/>
 	);
 }
