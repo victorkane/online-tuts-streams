c30275d7caca28e1dc5b39a1dcea36816c125d5e
victorkane <victorkane@gmail.com>
2023-03-18 10:41:48 -0300

    Create a Custom Block To Store and Display Custom Meta Fields

--- wp-content/plugins/metadata-block/metadata-block.php
+++ wp-content/plugins/metadata-block/metadata-block.php
@@ -1,11 +1,11 @@
-<?php
+ <?php
 /**
  * Plugin Name:       Meta Fields
- * Description:       Block Description
- * Requires at least: 6.1
+ * Description:       Block description
+ * Requires at least: 5.9
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            The Kinsta team
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       metadata-block
@@ -21,60 +21,139 @@
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function meta_fields_metadata_block_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type(
+		__DIR__ . '/build',
+		array(
+			'render_callback' => 'meta_fields_metadata_block_render_callback',
+		)
+	);
 }
 add_action( 'init', 'meta_fields_metadata_block_block_init' );
 
-// register meta box
-function meta_fields_add_meta_box(){
-	add_meta_box(
-		'meta_fields_meta_box', 
-		__( 'Book details' ), 
-		'meta_fields_build_meta_box_callback', 
-		'post',
-		'side',
-		'default',
-		// hide the meta box in Gutenberg
-		array('__back_compat_meta_box' => true)
-	 );
+/**
+ * Render callback function.
+ *
+ * @param array    $attributes The block attributes.
+ * @param string   $content    The block content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string The rendered output.
+ */
+function meta_fields_metadata_block_render_callback( $attributes, $content, $block ) {
+	$post_meta = get_post_meta( get_the_ID() );
+    
+    $output = "";
+
+    if( ! empty( $post_meta['_meta_fields_book_title'][0] ) ){
+        $title = '<h3>' . esc_html( $post_meta['_meta_fields_book_title'][0] ) . '</h3>';
+    } else {
+        $title = '<h3>' . esc_attr( get_the_title() ) . '</h3>';
+    }
+
+    if( ! empty( $post_meta['_meta_fields_book_author'][0] ) ){
+        $output .= '<li>' . __( 'Book author: ' ) . esc_html( $post_meta['_meta_fields_book_author'][0] ) . '</li>';
+    }
+    if( ! empty( $post_meta['_meta_fields_book_publisher'][0] ) ){
+        $output .= '<li>' . __( 'Book publisher: ' ) . esc_html( $post_meta['_meta_fields_book_publisher'][0] ) . '</li>';
+    }
+    if( ! empty( $post_meta['_meta_fields_book_date'][0] ) ){
+        $date = date_create( esc_html( $post_meta['_meta_fields_book_date'][0] ) );
+        $output .= '<li>' . __( 'Book date: ' ) . date_format( $date, "F d, Y" ) . '</li>';
+    }
+
+    if( strlen( $output ) > 0 ){
+        return '<div ' . get_block_wrapper_attributes() . '>' . $title . '<ul>' . $output . '</ul></div>'; 
+    } else {
+        return '<strong>' . __( 'Sorry. No fields available here!' ) . '</strong>';
+    }
 }
 
-// build meta box
+// register metabox
+function meta_fields_add_meta_box(){
+    add_meta_box(
+        'meta_fields_meta_box', 
+        __( 'Book details' ), 
+        'meta_fields_build_meta_box_callback', 
+        'post', 
+        'side',
+        'default',
+        // hide the metabox in Gutenberg
+        array('__back_compat_meta_box' => true)
+     );
+}
+
+// build metabox
 function meta_fields_build_meta_box_callback( $post ){
-	  wp_nonce_field( 'meta_fields_save_meta_box_data', 'meta_fields_meta_box_nonce' );
-	  $title = get_post_meta( $post->ID, '_meta_fields_book_title', true );
-	  $author = get_post_meta( $post->ID, '_meta_fields_book_author', true );
-	  ?>
-	  <div class="inside">
-	  	  <p><strong>Title</strong></p>
-		  <p><input type="text" id="meta_fields_book_title" name="meta_fields_book_title" value="<?php echo esc_attr( $title ); ?>" /></p>	
-		  <p><strong>Author</strong></p>
-		  <p><input type="text" id="meta_fields_book_author" name="meta_fields_book_author" value="<?php echo esc_attr( $author ); ?>" /></p>
-	  </div>
-	  <?php
+      wp_nonce_field( 'meta_fields_save_meta_box_data', 'meta_fields_meta_box_nonce' );
+      $author = get_post_meta( $post->ID, '_meta_fields_book_title', true );
+      $author = get_post_meta( $post->ID, '_meta_fields_book_author', true );
+      $publisher = get_post_meta( $post->ID, '_meta_fields_book_publisher', true );
+      $date = get_post_meta( $post->ID, '_meta_fields_book_date', true );
+      ?>
+      <div class="inside">
+          <p><strong>Title</strong></p>
+          <p><input type="text" id="meta_fields_book_title" name="meta_fields_book_title" value="<?php echo esc_attr( $title ); ?>" /></p>  
+          <p><strong>Author</strong></p>
+          <p><input type="text" id="meta_fields_book_author" name="meta_fields_book_author" value="<?php echo esc_attr( $author ); ?>" /></p>
+          <p><strong>Publisher</strong></p>
+          <p><input type="text" id="meta_fields_book_publisher" name="meta_fields_book_publisher" value="<?php echo esc_attr( $publisher ); ?>" /></p>
+          <p><strong>Date</strong></p>
+          <p><input type="date" id="meta_fields_book_date" name="meta_fields_book_date" value="<?php echo esc_attr( $date ); ?>" /></p>
+      </div>
+      <?php
 }
 add_action( 'add_meta_boxes', 'meta_fields_add_meta_box' );
 
-// save metadata
+// save meta data
 function meta_fields_save_meta_box_data( $post_id ) {
-	if ( ! isset( $_POST['meta_fields_meta_box_nonce'] ) )
-		return;
-	if ( ! wp_verify_nonce( $_POST['meta_fields_meta_box_nonce'], 'meta_fields_save_meta_box_data' ) )
-		return;
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-		return;
-	if ( ! current_user_can( 'edit_post', $post_id ) )
-		return;
+    if ( ! isset( $_POST['meta_fields_meta_box_nonce'] ) )
+        return;
+    if ( ! wp_verify_nonce( $_POST['meta_fields_meta_box_nonce'], 'meta_fields_save_meta_box_data' ) )
+        return;
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+    if ( ! current_user_can( 'edit_post', $post_id ) )
+        return;
 
-	if ( ! isset( $_POST['meta_fields_book_title'] ) )
-		return;
-	if ( ! isset( $_POST['meta_fields_book_author'] ) )
-		return;
+    if ( ! isset( $_POST['meta_fields_book_title'] ) )
+        return;
+    if ( ! isset( $_POST['meta_fields_book_author'] ) )
+        return;
+    if ( ! isset( $_POST['meta_fields_book_date'] ) )
+        return;
+    if ( ! isset( $_POST['meta_fields_book_publisher'] ) )
+        return;
+    
+    $title = sanitize_text_field( $_POST['meta_fields_book_title'] );
+    $author = sanitize_text_field( $_POST['meta_fields_book_author'] );
+    $date = sanitize_text_field( $_POST['meta_fields_book_date'] );
+    $publisher = sanitize_text_field( $_POST['meta_fields_book_publisher'] );
 
-	$title = sanitize_text_field( $_POST['meta_fields_book_title'] );
-	$author = sanitize_text_field( $_POST['meta_fields_book_author'] );
-
-	update_post_meta( $post_id, '_meta_fields_book_title', $title );
-	update_post_meta( $post_id, '_meta_fields_book_author', $author );
+    update_post_meta( $post_id, '_meta_fields_book_title', $title );
+    update_post_meta( $post_id, '_meta_fields_book_author', $author );
+    update_post_meta( $post_id, '_meta_fields_book_date', $date );
+    update_post_meta( $post_id, '_meta_fields_book_publisher', $publisher );
 }
 add_action( 'save_post', 'meta_fields_save_meta_box_data' );
+
+/**
+ * Register the custom meta fields
+ */
+function meta_fields_register_meta() {
+
+    $metafields = [ '_meta_fields_book_title', '_meta_fields_book_author', '_meta_fields_book_date', '_meta_fields_book_publisher' ];
+
+    foreach( $metafields as $metafield ){
+        // Pass an empty string to register the meta key across all existing post types.
+        register_post_meta( '', $metafield, array(
+            'show_in_rest' => true,
+            'type' => 'string',
+            'single' => true,
+            'sanitize_callback' => 'sanitize_text_field',
+            'auth_callback' => function() { 
+                return current_user_can( 'edit_posts' );
+            }
+        ));
+    }  
+}
+add_action( 'init', 'meta_fields_register_meta' );
--- wp-content/plugins/metadata-block/src/edit.js
+++ wp-content/plugins/metadata-block/src/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,7 +11,23 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InspectorControls,
+	RichText,
+} from "@wordpress/block-editor";
+
+import { useSelect } from "@wordpress/data";
+
+import { useEntityProp } from "@wordpress/core-data";
+
+import {
+	TextControl,
+	DatePicker,
+	TimePicker,
+	PanelBody,
+	PanelRow,
+} from "@wordpress/components";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -19,7 +35,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import "./editor.scss";
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -30,9 +46,108 @@ import './editor.scss';
  * @return {WPElement} Element to render.
  */
 export default function Edit() {
+	const blockProps = useBlockProps();
+
+	const postType = useSelect(
+		(select) => select("core/editor").getCurrentPostType(),
+		[]
+	);
+
+	const [meta, setMeta] = useEntityProp("postType", postType, "meta");
+
+	const bookTitle = meta["_meta_fields_book_title"];
+	const bookAuthor = meta["_meta_fields_book_author"];
+	const bookPublisher = meta["_meta_fields_book_publisher"];
+	const bookDate = meta["_meta_fields_book_date"];
+
+	const updateBookTitleMetaValue = (newValue) => {
+		setMeta({ ...meta, _meta_fields_book_title: newValue });
+	};
+
+	const updateBookAuthorMetaValue = (newValue) => {
+		setMeta({ ...meta, _meta_fields_book_author: newValue });
+	};
+
+	const updateBookPublisherMetaValue = (newValue) => {
+		setMeta({ ...meta, _meta_fields_book_publisher: newValue });
+	};
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Meta Fields – hello from the editor!', 'metadata-block' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={__("Book Details")} initialOpen={true}>
+					<PanelRow>
+						<fieldset>
+							<TextControl
+								label={__("Book title")}
+								value={bookTitle}
+								onChange={updateBookTitleMetaValue}
+							/>
+						</fieldset>
+					</PanelRow>
+					<PanelRow>
+						<fieldset>
+							<TextControl
+								label={__("Book author")}
+								value={bookAuthor}
+								onChange={updateBookAuthorMetaValue}
+							/>
+						</fieldset>
+					</PanelRow>
+					<PanelRow>
+						<fieldset>
+							<TextControl
+								label={__("Publisher")}
+								value={bookPublisher}
+								onChange={updateBookPublisherMetaValue}
+							/>
+						</fieldset>
+					</PanelRow>
+					<PanelRow>
+						<fieldset>
+							<DatePicker
+								currentDate={bookDate}
+								onChange={(newValue) =>
+									setMeta({ ...meta, _meta_fields_book_date: newValue })
+								}
+								__nextRemoveHelpButton
+								__nextRemoveResetButton
+							/>
+						</fieldset>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+			<div {...blockProps}>
+				<RichText
+					tagName="h3"
+					onChange={updateBookTitleMetaValue}
+					allowedFormats={["core/bold", "core/italic"]}
+					value={bookTitle}
+					placeholder={__("Write your text...")}
+				/>
+				<TextControl
+					label={__("Book author")}
+					value={bookAuthor}
+					onChange={updateBookAuthorMetaValue}
+				/>
+				<TextControl
+					label={__("Book publisher")}
+					value={bookPublisher}
+					onChange={updateBookPublisherMetaValue}
+				/>
+				{/*
+                    https://github.com/WordPress/gutenberg/tree/trunk/packages/components/src/date-time
+                    https://developer.wordpress.org/block-editor/reference-guides/components/date-time/
+                    */}
+				<TimePicker
+					currentTime={bookDate}
+					onChange={(newValue) =>
+						setMeta({ ...meta, _meta_fields_book_date: newValue })
+					}
+					__nextRemoveHelpButton
+					__nextRemoveResetButton
+				/>
+			</div>
+		</>
 	);
 }
