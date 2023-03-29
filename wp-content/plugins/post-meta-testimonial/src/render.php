5662d5571b7ea3748f750a05344d1fa63b2703cd
victorkane <victorkane@gmail.com>
2023-03-29 18:53:30 -0300

    feat(post-meta-testimonial): Support rendering post meta on the front end

--- wp-content/plugins/post-meta-testimonial/src/render.php
+++ wp-content/plugins/post-meta-testimonial/src/render.php
@@ -1,3 +1,16 @@
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Post Meta Testimonial – hello from a dynamic block!', 'post-meta-testimonial' ); ?>
-</p>
+<?php
+global $post;
+$testimonial = get_post_meta( $post->ID, 'testimonial', true );
+$author_name = $attributes['authorName'];
+$author_url  = $attributes['authorURL'];
+?>
+<blockquote <?php echo get_block_wrapper_attributes(); ?>>
+	<p><?php echo esc_html( $testimonial ); ?></p>
+	<cite>
+		<span><?php echo esc_html( $author_name ); ?></span>
+		<br />
+		<span>
+			<a href="<?php echo esc_url( $author_url ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html( $author_url ); ?></a>
+		</span>
+	</cite>
+</blockquote>
